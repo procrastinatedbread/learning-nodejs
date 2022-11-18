46f72b375f3fbec69067a0dd74eeaaa46caa9849
NIkhil Chhabra <nikhillchhabra@gmail.com>
2022-11-19 00:50:16 +0530

    Tried CRUD operations

--- index.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-fs.writeFileSync("read.txt", "helooooooooooooo");
-fs.writeFileSync("read.txt", "how are you?");
-fs.appendFileSync("read.txt", "helllloooooooooooo");
-const buf_data = fs.readFileSync("read.txt");
-console.log(buf_data);
-const org_data = buf_data.toString();
-console.log(org_data);
-fs.renameSync("read.txt", "readnikhil.txt");
---
+++ nodeChallenge/index.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+// fs.mkdirSync("Nikhil");
+
+// fs.writeFileSync("bio.txt", "My name is Nikhil");
+// fs.appendFileSync("bio.txt", ", I am an aspiring web developer.");
+// const data = fs.readFileSync("bio.txt", "utf8");
+// console.log(data);
+// fs.renameSync("bio.txt", "myBio.txt");
+// fs.unlinkSync("myBio.txt");
+fs.rmdirSync("Nikhil");
