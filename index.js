a5322f60d9d9ac415ea59e4bac4e6c8e998622d2
NIkhil Chhabra <nikhillchhabra@gmail.com>
2022-11-18 11:43:19 +0530

    learnt reading writing properties

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Welcome to my repository from where i am learning JS");
+const fs = require("fs");
+fs.writeFileSync("read.txt", "helooooooooooooo");
+fs.writeFileSync("read.txt", "how are you?");
+fs.appendFileSync("read.txt", "helllloooooooooooo");
+const buf_data = fs.readFileSync("read.txt");
+console.log(buf_data);
+const org_data = buf_data.toString();
+console.log(org_data);
+fs.renameSync("read.txt", "readnikhil.txt");
