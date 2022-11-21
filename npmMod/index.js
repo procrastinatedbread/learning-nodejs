43ca82119e1e94eccf14eb5a2f78d23db8d590ce
NIkhil Chhabra <nikhillchhabra@gmail.com>
2022-11-21 13:59:22 +0530

    npm chalk and validator

---
+++ npmMod/index.js
@@ -0,0 +1,10 @@
+// import chalk from "chalk";
+
+// console.log(chalk.blue("Hello world!"));
+import chalk from "chalk";
+// const validator = require("validator");
+import validator from "validator";
+console.log(chalk.red.underline.inverse("false"));
+// const res = validator.isEmail("nikhil@nikhilco.m");
+const res = validator.isEmail("nikhil@nikhil.com");
+console.log(res ? chalk.green.inverse(res) : chalk.red.inverse(res));
