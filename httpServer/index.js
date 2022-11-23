622001d815d681256ff9986e92a63da4fed72199
NIkhil Chhabra <nikhillchhabra@gmail.com>
2022-11-23 16:50:45 +0530

    api call

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,5 +1,8 @@
 const http = require("http");
+const fs = require("fs");
 const server = http.createServer((req, res) => {
+  const data = fs.readFileSync(`${__dirname}/userAPI/userapi.json`, "utf8");
+  const objData = JSON.parse(data);
   //   console.log(req.url);
   if (req.url == "/") {
     res.end("Hello from the other side ");
@@ -7,6 +10,9 @@ const server = http.createServer((req, res) => {
     res.end("Hello from the about us side ");
   } else if (req.url == "/contact") {
     res.end("Hello from the contact us page");
+  } else if (req.url == "/userapi") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(objData[2].name);
   } else {
     res.writeHead(404);
     res.end("<h1>404- page not found</h1>");
