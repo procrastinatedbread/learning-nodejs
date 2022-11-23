16569da6c6da6f4206d4fe125e146a1a5ec0f678
NIkhil Chhabra <nikhillchhabra@gmail.com>
2022-11-24 00:47:45 +0530

    streaming module

--- streamModule/index.js
+++ streamModule/index.js
@@ -4,15 +4,16 @@ const http = require("http");
 const server = http.createServer();
 server.on("request", (req, res) => {
   const rstream = fs.createReadStream("input.txt");
-  rstream.on("data", (chunkdata) => {
-    res.write(chunkdata);
-  });
-  rstream.on("end", () => {
-    res.end();
-  });
-  rstream.on("error", (err) => {
-    console.log(error);
-  });
+  //   rstream.on("data", (chunkdata) => {
+  //     res.write(chunkdata);
+  //   });
+  //   rstream.on("end", () => {
+  //     res.end();
+  //   });
+  //   rstream.on("error", (err) => {
+  //     console.log(error);
+  //   });
+  rstream.pipe(res);
 });
 
 server.listen(8000, "127.0.0.1");
